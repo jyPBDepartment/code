2664d02c7b7521b98062d3432dde2b7821f44047
zhouguohui <zhouguohui0328@163.com>
2020-05-06 11:38:53 +0800

    初始化项目

---
+++ jy_pc-1/src/main/java/com/jy/pc/Entity/UserEntity.java
@@ -0,0 +1,45 @@
+package com.jy.pc.Entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "user")
+public class UserEntity {
+
+	@Id
+	@GeneratedValue
+	private String userId;
+	@Column
+	private String userName;
+	@Column
+	private String userPassword;
+
+	public String getUserId() {
+		return userId;
+	}
+
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
+
+	public String getUserName() {
+		return userName;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+	public String getUserPassword() {
+		return userPassword;
+	}
+
+	public void setUserPassword(String userPassword) {
+		this.userPassword = userPassword;
+	}
+
+}
