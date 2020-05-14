7e7c09e8504daeb5750037f42b34ec2d66131692
admin <admin@DESKTOP-2DHFIMH>
2020-05-14 14:18:37 +0800

    控制层

--- jy_pc-1/src/main/java/com/jy/pc/controller/AdminController.java
+++ jy_pc-1/src/main/java/com/jy/pc/controller/AdminController.java
@@ -1,8 +1,10 @@
 package com.jy.pc.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping
 public class AdminController {
 
 }
