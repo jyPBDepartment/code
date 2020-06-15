227bca1bd40befe5f794255d6231dd88c1f02b70
zhouguohui <zhouguohui0328@163.com>
2020-06-15 08:18:45 +0800

    提交banner图管理时间时区问题

--- jy_system/src/main/java/com/jy/pc/Entity/BannerEntity.java
+++ jy_system/src/main/java/com/jy/pc/Entity/BannerEntity.java
@@ -32,11 +32,11 @@ public class BannerEntity {
 	@Column
 	private String url;
 	@Column
-	@JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+	@JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss",timezone="GMT+8")
     @Temporal(TemporalType.TIMESTAMP)
 	private Date createDate;
 	@Column
-	@JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+	@JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss",timezone="GMT+8")
     @Temporal(TemporalType.TIMESTAMP)
 	private Date updateDate;
 	@Column
