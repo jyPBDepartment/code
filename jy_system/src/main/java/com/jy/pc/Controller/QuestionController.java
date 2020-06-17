7ff3abe39f15cfff3aae692cf9e70ad78091cae8
zhouguohui <zhouguohui0328@163.com>
2020-06-17 08:40:32 +0800

    增加加密工具类

--- jy_system/src/main/java/com/jy/pc/Controller/QuestionController.java
+++ jy_system/src/main/java/com/jy/pc/Controller/QuestionController.java
@@ -18,97 +18,96 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.alibaba.fastjson.JSONObject;
 
-
-
-
 import com.jy.pc.Entity.QuestionEntity;
 import com.jy.pc.Service.QuestionService;
 
 @Controller
 @ResponseBody
-@RequestMapping(value="questionnaire")
+@RequestMapping(value = "questionnaire")
 public class QuestionController {
-@Autowired 
-private QuestionService questionService;
-		//调查问卷添加
-		@RequestMapping(value = "save")
-		public Map<String, String> save(HttpServletRequest res, HttpServletResponse req) {
-			Map<String, String> map = new HashMap<String, String>();
-			String s = res.getParameter("questionEntity");
-			JSONObject jsonObject = JSONObject.parseObject(s);
-			
-			Date date = new Date();
-			QuestionEntity questionEntity = jsonObject.toJavaObject(QuestionEntity.class);
-			questionEntity.setCreateDate(date);
+	@Autowired
+	private QuestionService questionService;
 
-			
-			if(questionEntity.getQuestionScore()>26) {
-				map.put("status", "0");
-				map.put("tips", "您的身份为供应商！");
-			}
-			if(questionEntity.getQuestionScore()>=20 && questionEntity.getQuestionScore()<=26) {
-				map.put("status", "1");
-				map.put("tips","您的身份为经销商！");
-			}
-			if(questionEntity.getQuestionScore()>=14 && questionEntity.getQuestionScore()<=19){
-				map.put("status", "2");
-				map.put("tips","您的身份为农业经理人！");
-			}
-			
-			
-			questionService.save(questionEntity);
-		
-			map.put("message", "添加成功");
-			return map;
+	// 调查问卷添加
+	@RequestMapping(value = "save")
+	public Map<String, String> save(HttpServletRequest res, HttpServletResponse req) {
+		Map<String, String> map = new HashMap<String, String>();
+		String s = res.getParameter("questionEntity");
+		JSONObject jsonObject = JSONObject.parseObject(s);
+
+		Date date = new Date();
+		QuestionEntity questionEntity = jsonObject.toJavaObject(QuestionEntity.class);
+		questionEntity.setCreateDate(date);
+
+		if (questionEntity.getQuestionScore() > 26) {
+			map.put("status", "0");
+			map.put("tips", "您的身份为供应商！");
 		}
-		// 调查问卷修改前查询
-				@RequestMapping(value = "findById")
-				public Map<String, Object> findById(HttpServletRequest res, HttpServletResponse req,
-						@RequestParam(name = "id") String id) {
-					Map<String, Object> map = new HashMap<String, Object>();
-					QuestionEntity question = questionService.findBId(id);
-					if (question != null) {
-						map.put("status", "0");// 查询数据成功
-						map.put("data", question);
-					} else {
-						map.put("status", "1");// 查询数据失败
-					}
-					return map;
-				}
-				// 调查问卷修改
-				@RequestMapping(value = "update")
-				public Map<String, String> update(HttpServletRequest res, HttpServletResponse req) {
-					Map<String, String> map = new HashMap<String, String>();
-					String s = res.getParameter("questionEntity");
-					JSONObject jsonObject = JSONObject.parseObject(s);
-					QuestionEntity questionEntity = jsonObject.toJavaObject(QuestionEntity.class);
-					questionService.update(questionEntity);
-					map.put("message", "修改成功");
-					return map;
-				}
-				// 调查问卷删除
-				@RequestMapping(value = "delete")
-				public Map<String, Object> delete(HttpServletRequest res, HttpServletResponse req,
-						@RequestParam(name = "id") String id) {
-					Map<String, Object> map = new HashMap<String, Object>();// 接收数据容器
-					questionService.delete(id);
-					map.put("status", "0");
-					map.put("message", "删除成功");
-					return map;
-				}
+		if (questionEntity.getQuestionScore() >= 20 && questionEntity.getQuestionScore() <= 26) {
+			map.put("status", "1");
+			map.put("tips", "您的身份为经销商！");
+		}
+		if (questionEntity.getQuestionScore() >= 14 && questionEntity.getQuestionScore() <= 19) {
+			map.put("status", "2");
+			map.put("tips", "您的身份为农业经理人！");
+		}
 
-				// 调查问卷模糊查询与分页
-				@RequestMapping(value = "findByName")
-				public Map<String, Object> findByName(HttpServletRequest res, HttpServletResponse req,
-						@RequestParam(name = "name") String name,@RequestParam(name = "phoneNum") String phoneNum,@RequestParam(name = "page") Integer page,
-						@RequestParam(name = "size") Integer size) {
-					Map<String, Object> map = new HashMap<String, Object>();
-					Pageable pageable = new PageRequest(page - 1, size);
-					Page<QuestionEntity> questionList = questionService.findListByName(name, phoneNum, pageable);
-					map.put("status", "0");// 成功
-					map.put("message", "查询成功");
-					map.put("data", questionList);
-					
-					return map;
-				}
+		questionService.save(questionEntity);
+
+		map.put("message", "添加成功");
+		return map;
+	}
+
+	// 分类修改前查询
+	@RequestMapping(value = "findById")
+	public Map<String, Object> findById(HttpServletRequest res, HttpServletResponse req,
+			@RequestParam(name = "id") String id) {
+		Map<String, Object> map = new HashMap<String, Object>();
+		QuestionEntity question = questionService.findBId(id);
+		if (question != null) {
+			map.put("status", "0");// 查询数据成功
+			map.put("data", question);
+		} else {
+			map.put("status", "1");// 查询数据失败
+		}
+		return map;
+	}
+
+	// 分类修改
+	@RequestMapping(value = "update")
+	public Map<String, String> update(HttpServletRequest res, HttpServletResponse req) {
+		Map<String, String> map = new HashMap<String, String>();
+		String s = res.getParameter("questionEntity");
+		JSONObject jsonObject = JSONObject.parseObject(s);
+		QuestionEntity questionEntity = jsonObject.toJavaObject(QuestionEntity.class);
+		questionService.update(questionEntity);
+		map.put("message", "修改成功");
+		return map;
+	}
+
+	// 分类删除
+	@RequestMapping(value = "delete")
+	public Map<String, Object> delete(HttpServletRequest res, HttpServletResponse req,
+			@RequestParam(name = "id") String id) {
+		Map<String, Object> map = new HashMap<String, Object>();// 接收数据容器
+		questionService.delete(id);
+		map.put("status", "0");
+		map.put("message", "删除成功");
+		return map;
+	}
+
+	// 分类模糊查询与分页
+	@RequestMapping(value = "findByName")
+	public Map<String, Object> findByName(HttpServletRequest res, HttpServletResponse req,
+			@RequestParam(name = "name") String name, @RequestParam(name = "phoneNum") String phoneNum,
+			@RequestParam(name = "page") Integer page, @RequestParam(name = "size") Integer size) {
+		Map<String, Object> map = new HashMap<String, Object>();
+		Pageable pageable = new PageRequest(page - 1, size);
+		Page<QuestionEntity> questionList = questionService.findListByName(name, phoneNum, pageable);
+		map.put("status", "0");// 成功
+		map.put("message", "查询成功");
+		map.put("data", questionList);
+
+		return map;
+	}
 }
--- jy_system/src/main/java/com/jy/pc/Service/impl/QuestionServiceImpl.java
+++ jy_system/src/main/java/com/jy/pc/Service/impl/QuestionServiceImpl.java
@@ -11,16 +11,25 @@ import com.alibaba.fastjson.JSONObject;
 import com.jy.pc.DAO.QuestionDao;
 import com.jy.pc.Entity.QuestionEntity;
 import com.jy.pc.Service.QuestionService;
+import com.jy.pc.Utils.Aes;
 
 @Service
-public class QuestionServiceImpl implements QuestionService{
+public class QuestionServiceImpl implements QuestionService {
 	@Autowired
 	private QuestionDao questionDao;
 
 	@Override
 	public QuestionEntity save(QuestionEntity questionEntity) {
 
-		JSONArray jsonArray = JSON.parseArray(questionEntity.getQuestionAnswer());
+		String qa = questionEntity.getQuestionAnswer();
+		Aes aes = new Aes();
+		String qus = "";
+		try {
+			qus = aes.desEncrypt(qa);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		JSONArray jsonArray = JSON.parseArray(qus);
 		int score = 0;
 		for (int i = 0; i < jsonArray.size(); i++) {
 			JSONObject jo = JSON.parseObject(jsonArray.get(i).toString());
@@ -63,21 +72,20 @@ public class QuestionServiceImpl implements QuestionService{
 				if (jo.get("value").toString().indexOf("B") > 0) {
 					score = score + 3;
 				}
-				
+
 			}
 			if (i == 4) {
-				if (jo.get("value").toString().indexOf("A") > 0 || jo.get("value").toString().indexOf("B") > 0 
-						 || jo.get("value").toString().indexOf("C") > 0 || jo.get("value").toString().indexOf("D") > 0 
-						 || jo.get("value").toString().indexOf("E") > 0 || jo.get("value").toString().indexOf("F") > 0
-						 || jo.get("value").toString().indexOf("G") > 0 || jo.get("value").toString().indexOf("H") > 0
-						 || jo.get("value").toString().indexOf("I") > 0
-						) {
+				if (jo.get("value").toString().indexOf("A") > 0 || jo.get("value").toString().indexOf("B") > 0
+						|| jo.get("value").toString().indexOf("C") > 0 || jo.get("value").toString().indexOf("D") > 0
+						|| jo.get("value").toString().indexOf("E") > 0 || jo.get("value").toString().indexOf("F") > 0
+						|| jo.get("value").toString().indexOf("G") > 0 || jo.get("value").toString().indexOf("H") > 0
+						|| jo.get("value").toString().indexOf("I") > 0) {
 					score = score + 5;
 				}
 				if (jo.get("value").toString().indexOf("J") > 0) {
 					score = score + 3;
 				}
-				
+
 			}
 			if (i == 5) {
 				if (jo.get("value").toString().indexOf("A") > 0 || jo.get("value").toString().indexOf("B") > 0) {
@@ -86,39 +94,40 @@ public class QuestionServiceImpl implements QuestionService{
 				if (jo.get("value").toString().indexOf("C") > 0 || jo.get("value").toString().indexOf("D") > 0) {
 					score = score + 3;
 				}
-				
+
 			}
 
 		}
+
+		questionEntity.setQuestionAnswer("");
 		questionEntity.setQuestionScore(score);
 
-		return questionDao.save(questionEntity);
-
+		return questionDao.saveAndFlush(questionEntity);
 
 	}
 
 	@Override
 	public void update(QuestionEntity questionEntity) {
 		questionDao.saveAndFlush(questionEntity);
-		
+
 	}
 
 	@Override
 	public void delete(String id) {
 		questionDao.deleteById(id);
-		
+
 	}
 
 	@Override
 	public QuestionEntity findBId(String id) {
-		
+
 		return questionDao.findBId(id);
 	}
 
 	@Override
 	public Page<QuestionEntity> findListByName(String name, String phoneNum, Pageable pageable) {
-		String questionName = "%"+name+"%";
-		String questionTel = "%"+phoneNum+"%";
+		String questionName = "%" + name + "%";
+		String questionTel = "%" + phoneNum + "%";
 		return questionDao.findListByName(questionName, questionTel, pageable);
 	}
 
