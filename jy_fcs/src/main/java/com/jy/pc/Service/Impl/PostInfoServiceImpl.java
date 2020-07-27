0f9a54756d764d5ed627dab498d8991f8863bde5
aleileia <2070184528@qq.com>
2020-07-27 16:47:15 +0800

    修改帖子管理名称

---
+++ jy_fcs/src/main/java/com/jy/pc/Service/Impl/PostInfoServiceImpl.java
@@ -0,0 +1,40 @@
+package com.jy.pc.Service.Impl;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
+import com.jy.pc.DAO.PostInfoDao;
+import com.jy.pc.Entity.PostInfoEntity;
+import com.jy.pc.Service.PostInfoService;
+
+@Service
+
+public class PostInfoServiceImpl implements PostInfoService{
+
+	@Autowired
+	private PostInfoDao invitationDao ;
+	@Override
+	public List<PostInfoEntity> findInva() {
+		
+		return invitationDao.findInva();
+	}
+	@Override
+	public Page<PostInfoEntity> findListByName(String name, String createUser, Pageable pageable) {
+		String invName = "%"+name+"%";
+		String user = "%"+createUser+"%";
+		return invitationDao.findListByName(invName, user, pageable);
+	}
+	@Override
+	public PostInfoEntity findId(String id) {
+		return invitationDao.findId(id);
+	}
+	@Override
+	public void update(PostInfoEntity invitation) {
+		invitationDao.saveAndFlush(invitation);
+	}
+
+}
