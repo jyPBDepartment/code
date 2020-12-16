4408d35f784f3f47d314aeb695a589fc862e8550
duanxiaodong <1820146886@qq.com>
2020-12-16 16:30:37 +0800

    对大于1M的图片进行压缩后，再上传至ftp服务器

--- jy_fcs/src/main/java/com/jy/pc/Controller/UploaderController.java
+++ jy_fcs/src/main/java/com/jy/pc/Controller/UploaderController.java
@@ -1,5 +1,7 @@
 package com.jy.pc.Controller;
 
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.HashMap;
@@ -8,8 +10,10 @@ import java.util.Map;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.apache.http.entity.ContentType;
 import org.joda.time.DateTime;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.mock.web.MockMultipartFile;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -18,6 +22,8 @@ import org.springframework.web.multipart.MultipartFile;
 
 import com.jy.pc.Utils.FtpUtil;
 import com.jy.pc.Utils.IDUtils;
+
+import net.coobird.thumbnailator.Thumbnails;
 
 @Controller
 @RequestMapping(value = "/upload")
@@ -47,6 +53,12 @@ public class UploaderController {
 			HttpServletRequest request, HttpServletResponse response) {
 		Map<String, Object> map = new HashMap<String, Object>();
 		try {
+			byte[] imgBytes = uploadFile.getBytes();
+			byte[] resultBytes = compressPicForScale(imgBytes, 1024 * 5);
+//			System.out.println("压缩前：" + uploadFile.getSize());
+			InputStream inputStream = new ByteArrayInputStream(resultBytes);
+			MultipartFile file = new MockMultipartFile(ContentType.APPLICATION_OCTET_STREAM.toString(), inputStream);
+//			System.out.println("压缩后：" + file.getSize());
 			// 1、给上传的图片生成新的文件名
 			// 1.1获取原始文件名
 			String oldName = uploadFile.getOriginalFilename();
@@ -65,7 +77,7 @@ public class UploaderController {
 
 			// 3、把图片上传到图片服务器
 			// 3.1获取上传的io流
-			InputStream input = uploadFile.getInputStream();
+			InputStream input = file.getInputStream();
 
 			// 3.2调用FtpUtil工具类进行上传
 			boolean result = FtpUtil.uploadFile(host, port, userName, passWord, basePath, filePath, newName, input);
@@ -80,6 +92,46 @@ public class UploaderController {
 		}
 
 		return map;
+	}
+
+	/**
+	 * 自动调节精度(经验数值)
+	 *
+	 * @param size 源图片大小
+	 * @return 图片压缩质量比
+	 */
+	private static double getAccuracy(long size) {
+		double accuracy;
+		if (size < 900) {
+			accuracy = 0.85;
+		} else if (size < 2047) {
+			accuracy = 0.6;
+		} else if (size < 3275) {
+			accuracy = 0.44;
+		} else {
+			accuracy = 0.4;
+		}
+		return accuracy;
+	}
+
+	public static byte[] compressPicForScale(byte[] imageBytes, long desFileSize) {
+		if (imageBytes == null || imageBytes.length <= 0 || imageBytes.length < desFileSize * 1024) {
+			// 若传入图片已小于目标大小，则不作处理
+			return imageBytes;
+		}
+		long srcSize = imageBytes.length;
+		double accuracy = getAccuracy(srcSize / 1024);
+		try {
+			while (imageBytes.length > desFileSize * 1024) {
+				ByteArrayInputStream inputStream = new ByteArrayInputStream(imageBytes);
+				ByteArrayOutputStream outputStream = new ByteArrayOutputStream(imageBytes.length);
+				Thumbnails.of(inputStream).scale(accuracy).outputQuality(accuracy).toOutputStream(outputStream);
+				imageBytes = outputStream.toByteArray();
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return imageBytes;
 	}
 
 }
