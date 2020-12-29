7043c86e9eca1ef425601a2273c74957c798b8ed
zhouguohui <zhouguohui0328@163.com>
2020-12-29 08:56:35 +0800

    合并代码

--- jy_fcs/src/main/java/com/jy/pc/JyFcsApplication.java
+++ jy_fcs/src/main/java/com/jy/pc/JyFcsApplication.java
@@ -2,16 +2,14 @@ package com.jy.pc;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.aspectj.EnableSpringConfigured;
 
 @SpringBootApplication
 @EnableSpringConfigured
-@EnableConfigurationProperties
 public class JyFcsApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(JyFcsApplication.class, args);
 	}
-
+	
 }
