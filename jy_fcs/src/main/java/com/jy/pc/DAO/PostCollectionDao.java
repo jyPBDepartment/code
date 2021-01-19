67d894a8232f7e54b7981d7b322cdedf29d7f902
JXNxixi <1084562297@qq.com>
2021-01-19 16:38:49 +0800

    1.修改我的收藏排序功能

--- jy_fcs/src/main/java/com/jy/pc/DAO/PostCollectionDao.java
+++ jy_fcs/src/main/java/com/jy/pc/DAO/PostCollectionDao.java
@@ -21,8 +21,8 @@ public interface PostCollectionDao extends JpaRepository<PostCollectionEntity, S
 	 * 
 	 * @Param userId 用户Id
 	 */
-	@Query(value = "select t.id,t.code,(select GROUP_CONCAT(a.pic_url) from sas_picture_info a,sas_post_picture a1,sas_post_info a2 where a.id = a1.photo_id and a2.id = a1.post_id AND a2.id = t.id ) as picture,t.comment_num as commentNum,t.author,t.audit_optinion as auditOptinion,t.parent_code as parentCode,t.is_selected as isSelected,t.collection_num as collectionNum,t.comment_id as commentId,t.name,t.status,t.create_user_id as createUserId,t.create_date as createDate,t.update_date as updateDate,t.audit_user as auditUser,t.visibility,t.praise_num as praiseNum,t.is_anonymous as isAnonymous,t.reason,t.audit_status as auditStatus,t.create_user as createUser,t.header,t.pageview from sas_post_info t,sas_post_collection t1 where t.id = t1.circle_id and t1.user_id =:userId",
-			countQuery = "select count(0) from sas_post_info t,sas_post_collection t1 where t.id =t1.circle_id and t1.user_id =:userId", nativeQuery = true)
+	@Query(value = "select t.id,t.code,(select GROUP_CONCAT(a.pic_url) from sas_picture_info a,sas_post_picture a1,sas_post_info a2 where a.id = a1.photo_id and a2.id = a1.post_id AND a2.id = t.id ) as picture,t.comment_num as commentNum,t.author,t.audit_optinion as auditOptinion,t.parent_code as parentCode,t.is_selected as isSelected,t.collection_num as collectionNum,t.comment_id as commentId,t.name,t.status,t.create_user_id as createUserId,t.create_date as createDate,t.update_date as updateDate,t.audit_user as auditUser,t.visibility,t.praise_num as praiseNum,t.is_anonymous as isAnonymous,t.reason,t.audit_status as auditStatus,t.create_user as createUser,t.header,t.pageview from sas_post_info t,sas_post_collection t1 where t.id = t1.circle_id and t1.user_id =:userId order by t.create_date desc",
+			countQuery = "select count(0) from sas_post_info t,sas_post_collection t1 where t.id =t1.circle_id and t1.user_id =:userId order by t.create_date desc", nativeQuery = true)
 	public Page<List<Map<String, Object>>> findPageByParam(String userId, Pageable pageable);
 
 }
